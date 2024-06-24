d331ef70afc381a3bfff13444e0331be1e0feded
prajapati-yash <prajapatiyash.py30@gmail.com>
2024-06-24 16:44:30 +0530

    Updated the api

--- api.py
+++ api.py
@@ -91,8 +91,7 @@ def analyze_video(video: VideoURL):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
 if __name__ == "__main__":
-    import uvicorn
-    host = os.getenv("HOST", "0.0.0.0")
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host=host, port=port)
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("api:app", host="0.0.0.0", port=port, log_level="info")
